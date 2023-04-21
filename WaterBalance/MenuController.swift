9adb58902bf7939d50d34b734e58fcee13657939
alexa <allexa.msv@gmail.com>
2023-04-21 17:15:03 +0300

    The code has been cleaned up and added an entity for CoreData

--- WaterBalance/MenuController.swift
+++ WaterBalance/MenuController.swift
@@ -11,38 +11,15 @@ class MenuController: UIViewController {
     weak var delegate: MenuControllerDelegate?
     var selectedSegment = "100ml"
     
-    
     @IBOutlet weak var menuView: UIView!
     @IBOutlet weak var segmentedcontrol: UISegmentedControl!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         menuView.layer.cornerRadius = 20
         segmentedcontrol.setTitleTextAttributes([.foregroundColor: UIColor.white], for: .selected)
         segmentedcontrol.setTitleTextAttributes([.foregroundColor: UIColor(named: "darkBlueColor")!], for: .normal)
-      //  view.layer.masksToBounds = true
     }
-
-    
-    //The minimum step of adding water is 50 ml, which is equal to reducing the growth variable by 40. The filled drop is the value of the variable is 50, and the empty drop is 850
-    
-    @IBAction func add100ml(_ sender: Any) {
-        delegate?.changeInf(5.0, number2: 40)
-        dismiss(animated: true, completion: nil)
-    }
-    
-    @IBAction func add250ml(_ sender: Any) {
-        delegate?.changeInf(12.5, number2: 100)
-        dismiss(animated: true, completion: nil)
-    }
-    
-    
-    @IBAction func add500ml(_ sender: Any) {
-        delegate?.changeInf(25.0, number2: 200)
-        dismiss(animated: true, completion: nil)
-    }
-    
     
     @IBAction func selectedWaterVolume(_ sender: UISegmentedControl) {
         switch sender.selectedSegmentIndex {
@@ -54,7 +31,6 @@ class MenuController: UIViewController {
         }
     }
     
-    
     @IBAction func addWater(_ sender: Any) {
         if selectedSegment == "100ml" {delegate?.changeInf(5.0, number2: 40)}
         if selectedSegment == "250ml" {delegate?.changeInf(12.5, number2: 100)}
@@ -63,15 +39,11 @@ class MenuController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
-    
     @IBAction func back(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-    
-    
 }
 
 protocol MenuControllerDelegate: AnyObject {
     func changeInf(_ number1: Double, number2: Int)
-    
 }
--- WaterBalance/ViewController.swift
+++ WaterBalance/ViewController.swift
@@ -7,41 +7,36 @@
 
 import UIKit
 class ViewController: UIViewController, MenuControllerDelegate {
-    
-    
     var percent: Double = 0
     
-        @IBOutlet weak var Wave: WaveView!
-        @IBOutlet weak var DropLet: UIImageView!
-        @IBOutlet weak var WaterDisplay: UILabel!
+    @IBOutlet weak var Wave: WaveView!
+    @IBOutlet weak var DropLet: UIImageView!
+    @IBOutlet weak var WaterDisplay: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateWaterDisplay()
         
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            updateWaterDisplay()
-            
-            DropLet.layer.cornerRadius = 20
-            DropLet.layer.shadowRadius = 20
-          //  WaterDisplay.text = "\(Int(percent.rounded()))%"
-            
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                self.Wave.animationStart(direction: .right, speed: 10)
-                
-            }
-           
+        DropLet.layer.cornerRadius = 20
+        DropLet.layer.shadowRadius = 20
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.Wave.animationStart(direction: .right, speed: 10)
         }
+    }
+    
+    func updateWaterDisplay() {
+        WaterDisplay.text = "\(Int(percent.rounded()))%"
+    }
+    
+    
+    @IBAction func showAddWaterMenu(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let menu = storyboard.instantiateViewController(withIdentifier: "menu") as? MenuController else { return }
+        menu.delegate = self
+        present(menu, animated: true, completion: nil)
         
-        func updateWaterDisplay() {
-            WaterDisplay.text = "\(Int(percent.rounded()))%"
-        }
-        
-        
-        @IBAction func showAddWaterMenu(_ sender: Any) {
-            let storyboard = UIStoryboard(name: "Main", bundle: nil)
-            guard let menu = storyboard.instantiateViewController(withIdentifier: "menu") as? MenuController else { return }
-            menu.delegate = self
-            present(menu, animated: true, completion: nil)
-            
-        }
+    }
     func changeInf(_ number1: Double, number2: Int) {
         self.percent += number1
         if self.percent >= 100 {self.percent = 100}
@@ -50,42 +45,7 @@ class ViewController: UIViewController, MenuControllerDelegate {
         
         updateWaterDisplay()
     }
-        
-        
-        
-        /*
-       
-         @IBAction func add100(_ sender: Any) {
-         //case: (almost) full
-         if Wave.hight < 90 {Wave.hight = 50}
-         //case: append
-         else {Wave.hight -= 40}
-         percent += 5
-         WaterDisplay.text = "\(Int(percent.rounded()))%"
-         }
-         
-        
-         @IBAction func add250(_ sender: Any) {
-         //case: (almost) full
-         if Wave.hight < 150 {Wave.hight = 50}
-         //case: append
-         else {Wave.hight -= 100}
-         percent += 12.5
-         WaterDisplay.text = "\(Int(percent.rounded()))%"
-         
-         }
-         
-         
-         @IBAction func add500(_ sender: Any) {
-         //case: (almost) full
-         if Wave.hight < 250 {Wave.hight = 50}
-         //case: append
-         else {Wave.hight -= 200}
-         percent += 25
-         WaterDisplay.text = "\(Int(percent.rounded()))%"
-         }
-         */
-    }
+}
     
 
 
